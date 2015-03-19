c6dd63ce5363f50f2579e07b821115f03a7d13ce
Alexey Tsymlov <alexeytsymlov@Alexeys-MacBook-Pro.local>
2015-03-19 18:37:50 +0300

    After fifth lecture.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Alexey Tsymlov on 3/19/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
