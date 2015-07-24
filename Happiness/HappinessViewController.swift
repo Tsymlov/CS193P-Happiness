e3cb98382103449682acfc4b764bca4ba333d65b
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-07-24 19:41:38 +0300

    After sixth lecture.

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -13,6 +13,7 @@ class HappinessViewController: UIViewController, FaceViewDataSource {
     @IBOutlet weak var faceView: FaceView!{
         didSet{
             faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
         }
     }
     
@@ -31,5 +32,23 @@ class HappinessViewController: UIViewController, FaceViewDataSource {
     
     func smilinessForFaceView(sender: FaceView) -> Double? {
         return Double(happiness-50)/50
+    }
+    
+    struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state{
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
     }
 }
